3aaecfa3357892a5f66ef2c8083e88dc75f76540
hieucaotrantrong <hieu98861@donga.edu.vn>
2025-11-23 13:51:33 +0700

    version1

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,15 +1,17 @@
 import dotenv from "dotenv";
-dotenv.config();   // ĐẢM BẢO .env load TRƯỚC
+dotenv.config();
 
 import { Pool } from "pg";
 
 const pool = new Pool({
-    host: process.env.PG_HOST || "localhost",
-    user: process.env.PG_USER || "postgres",
-    password: process.env.PG_PASSWORD || "",
-    database: process.env.PG_DATABASE || "clothes_db",
-    port: Number(process.env.PG_PORT) || 5432,
-    ssl: false
+    host: process.env.PG_HOST,
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    database: process.env.PG_DATABASE,
+    port: Number(process.env.PG_PORT),
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 export default pool;
